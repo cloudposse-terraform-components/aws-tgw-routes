6bca876dec60459cfbb0c3443a082efeba745cdf
milldr <miller0daniel@gmail.com>
2026-01-22 11:14:30 -0500

    fix: replace mock outputs with meaningful resource outputs

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "transit_gateway_route_table_propagations" {
+  description = "Transit Gateway route table propagations"
+  value       = aws_ec2_transit_gateway_route_table_propagation.this
+}
+
+output "transit_gateway_route_table_associations" {
+  description = "Transit Gateway route table associations"
+  value       = aws_ec2_transit_gateway_route_table_association.this
+}
+
+output "transit_gateway_routes" {
+  description = "Transit Gateway static routes"
+  value       = aws_ec2_transit_gateway_route.this
 }
